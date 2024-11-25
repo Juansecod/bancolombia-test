20c8b4562d17c707b30a6d16f6c17b2c0a3c3b18
juansecod <juan.sebastianri28@gmail.com>
2024-11-25 00:15:56 -0500

    Implements Exception handlers to form and periods to predict

--- prediccion_tmr/views.py
+++ prediccion_tmr/views.py
@@ -4,34 +4,38 @@ from .utils import generate_plots, train_model
 
 def home(request):
     """
-    The `home` function processes a file uploaded through a web request, reads the data from the file
-    either as a CSV or Excel file, trains a model on the data, generates forecasts, and returns the
-    predictions and plots to be displayed on a webpage.
+    The `home` function in Python processes a POST request to read a CSV or Excel file, train a model,
+    generate forecasts, and display the results on a webpage.
     
     :param request: The `request` parameter in the `home` function is typically an object that contains
     information about the current HTTP request. It includes details such as the request method (GET,
-    POST, etc.), any data sent with the request (such as form data or files), and other metadata related
+    POST, etc.), any data sent in the request (such as form data or files), and other metadata related
     to the request
-    :return: The `home` function returns a rendered HTML template named "index.html" with the context
-    data that includes the title "Prediccion TMR", predictions data, and plots data. The function first
-    checks if the request method is POST, reads the uploaded file (either CSV or Excel), trains a model
-    using the data, generates predictions, and plots based on the forecast data. The predictions are
-    then
+    :return: The `home` function is returning a rendered HTML template named "index.html" with the
+    context data that includes the title, predicts, plots, and error messages based on the logic and
+    exceptions handled within the function.
     """
     context = {
         "title":"Prediccion TMR", 
     }
-    
-    if request.method == "POST":
-        file = request.FILES.get('file')
-        if file.content_type == "text/csv":
-            df = pd.read_csv(file)
-        elif file.content_type == "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet":
-            df = pd.read_excel(file)
+    try:
+        if request.method == "POST":
+            freq = int(request.POST["freq"])
+            file = request.FILES.get('file')
+            if file.content_type == "text/csv":
+                df = pd.read_csv(file)
+            elif file.content_type == "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet":
+                df = pd.read_excel(file)
 
-        forecast = train_model(df)
-        forecast["yhat"] = round(forecast["yhat"], 2)
-        context["predicts"] = forecast.tail(30).to_dict('records')
-        context["plots"] = generate_plots(forecast)
+            forecast = train_model(df, freq)
+            forecast["yhat"] = round(forecast["yhat"], 2)
+            context["predicts"] = forecast.tail(freq).to_dict('records')
+            context["plots"] = generate_plots(forecast)
+    except KeyError:
+        context['error'] = "Ups, algo no ha funcionado bien. Intenta verificar si el dataset usado tiene los campos necesarios."
+    except AttributeError:
+        context['error'] = "Ups, parece que se te ha olvidado algo. Recuerda subir el dataset"
+    except Exception:
+        context['error'] = "Ups, parece algo salio mal"
     
     return render(request,"index.html",context)
